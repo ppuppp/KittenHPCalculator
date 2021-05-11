44afe1e837f06d147f3e7baf78c0dd32aff1a7ae
Gabriel Puppi <puppizin@gmail.com>
2021-05-11 16:15:21 -0300

    Initial Commit

---
+++ KittyHPCalculator/KittyHPCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittyHPCalculatorApp.swift
+//  KittyHPCalculator
+//
+//  Created by Gabriel Puppi on 11/05/21.
+//
+
+import SwiftUI
+
+@main
+struct KittyHPCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
